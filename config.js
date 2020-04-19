6e2b0110b0cbad5639b9634ec021a422a9ff5bcd
kmtrcong <63954640+kmtrcong@users.noreply.github.com>
2020-04-19 18:22:27 +0700

    Add files via upload
    Initialization

---
+++ config.js
@@ -0,0 +1,11 @@
+/*
+* Theasys
+* Generated on: Friday 17th of April 2020 03:22:04 AM
+* Config file: You can change the path to match your server path.
+*/
+
+var vars = {
+    file : 'index.html',
+    path : 'https://duynqvn.github.io/rcong/',//path to the index.html
+    theme : 'theasys',
+};
